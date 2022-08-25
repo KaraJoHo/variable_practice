57045570cf329f53d41793d61eb50a6c9c8c441f
Kara <karajones-hofmann@Karas-MacBook-Pro.local>
2022-08-25 11:14:41 -0600

    Reassigned values

--- variables.rb
+++ variables.rb
@@ -1,3 +1,5 @@
+##Variables
+
 my_name = "Kara"
 my_mood ="Happy"
 
@@ -9,3 +11,17 @@ another_decimal_point = 2.2
 
 currently_happy = true
 currently_sad = false
+
+##Reassigned
+
+my_name = "Joe"
+my_mood = "Sad"
+
+my_age = 30
+a_hundred = 200
+
+decimal_point = 1.01
+another_decimal_point = 2.02
+
+currently_happy = false
+currently_sad = true
